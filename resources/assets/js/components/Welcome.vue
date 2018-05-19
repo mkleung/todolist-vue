916c1e244c006dae0638c0464d10eb4899d17b15
Mike Leung <mpakleung@gmail.com>
2018-05-19 12:00:19 -0400

    Test git

--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -11,7 +11,7 @@
                     <!-- SAERCH -->
                     <div class="card-content">
                         <div class="has-text-centered">
-                            <h1>Welcome</h1>
+                            <h1>Welcome!</h1>
                             <router-link class="button is-large is-danger" to="/dashboard">Dashboard</router-link>
                         </div>
                      </div>
