527579972535ac94bd4436ffde3246c17a47e5f2
Mike Leung <mpakleung@gmail.com>
2018-05-19 15:19:15 -0400

    Add social media icons in footer

--- resources/assets/js/components/AppFooter.vue
+++ resources/assets/js/components/AppFooter.vue
@@ -2,6 +2,10 @@
     <footer class="footer">
         <div class="container">
             <div class="content has-text-centered">
+                <a hef=""><span class="icon has-text-info"><i class="fa fa-facebook fa-2x"></i></span></a>
+                <a hef=""><span class="icon has-text-success"><i class="fa fa-twitter fa-2x"></i></span></a>
+                <a hef=""><span class="icon has-text-warning"><i class="fa fa-instagram fa-2x"></i></span></a>
+                <a hef=""><span class="icon has-text-danger"><i class="fa fa-reddit fa-2x"></i></span></a>
             <p>
                 <strong>Coffee To Do</strong> by <a href="http://mikeleung.ca" target="_blank">Mike Leung</a>
             </p>
