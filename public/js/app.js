42435521c6e64db804f87498a510198812d57a9d
Mike L <mpakleung@gmail.com>
2018-05-26 15:20:08 -0400

    More refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -45334,99 +45334,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 var register = __webpack_require__(4);
@@ -45807,7 +45714,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("section", [
-    _c("div", { staticClass: "hero is-medium is-bold" }, [
+    _c("div", { staticClass: "hero is-large is-bold" }, [
       _c("div", { staticClass: "hero-body" }, [
         _c("div", { staticClass: "container" }, [
           _c(
@@ -45822,7 +45729,7 @@ var render = function() {
               _vm._v(" "),
               _c("h2", { staticClass: "subtitle " }, [
                 _vm._v(
-                  "\n                        Coffee dates with people who love to do lists\n                    "
+                  "\n                        To Do List over coffee\n                    "
                 )
               ]),
               _vm._v(" "),
--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -2,7 +2,7 @@
 
 
 <section >
-    <div class="hero is-medium is-bold">
+    <div class="hero is-large is-bold">
         <div class="hero-body">
             <div class="container">
                 <div class="column is-6 is-offset-3 has-text-centered ">
@@ -10,107 +10,14 @@
                         CoffeeToDo
                     </h1>
                     <h2 class="subtitle ">
-                        Coffee dates with people who love to do lists
+                        To Do List over coffee
                     </h2>
                     <register :welcome="welcome"></register>
                 </div>
             </div>
         </div>
     </div>
-<!--
-    <div class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns">
-                    <div class="column is-4  has-text-centered">
-                        <p class="title">
-                            For work addicts
-                        </p>
-                        <img :src="'/img/coffee1.jpg'" >
-                    </div>
 
-                    <div class="column is-4  has-text-centered">
-                        <p class="title">
-                            Who need to get things done
-                        </p>
-                        <img :src="'/img/coffee2.jpg'" >
-                    </div>
-
-                    <div class="column is-4  has-text-centered">
-                        <p class="title">
-                          With other people
-                        </p>
-                        <img :src="'/img/coffee3.jpg'" >
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-        <div class="hero is-bold">
-            <div class="hero-body">
-                <div class="container is-fluid">
-                    <div class="columns ">
-
-                            <div class="column is-3 is-offset-3 has-text-right">
-                                <h1 class="title">
-                                    Anyone can join in
-                                </h1>
-                                <p>Productivity junkies who want to meet similarly minded people</p>
-                            </div>
-
-                            <div class="column is-3">
-                                <figure class="image ">
-                                    <img :src="'/img/coffee4.jpg'" >
-                                </figure>
-                            </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-        <div class="hero is-light">
-            <div class="hero-body">
-                <div class="container is-fluid">
-                     <div class="columns">
-                         <div class="column is-3 is-offset-3">
-                            <figure class="image ">
-                                <img :src="'/img/coffee5.jpg'">
-                            </figure>
-                        </div>
-
-                        <div class="column is-3">
-                            <h1 class="title">
-                                You need help in something?
-                            </h1>
-                            <p>Collaborate and do things</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        
-        <div class="hero is-small is-bold">
-            <div class="hero-body">
-                <div class="container">
-                    <div class="column is-6 is-offset-3 has-text-centered ">
-                        <h1 class="title">
-                            It's free forever
-                        </h1>
-                        <h2 class="subtitle ">
-                            Meet someone over coffee and get things done
-                        </h2>
-                        <register :welcome="welcome"></register>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-    
 </section>
 
 </template>
