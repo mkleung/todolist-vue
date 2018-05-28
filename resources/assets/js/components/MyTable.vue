77eee2a65a87182253acd3e0dc38db27dfa93a48
Mike Leung <mpakleung@gmail.com>
2018-05-27 23:20:01 -0400

    Start working on table

--- resources/assets/js/components/MyTable.vue
+++ resources/assets/js/components/MyTable.vue
@@ -6,13 +6,27 @@
 
       
             <b-table
-
+                :data="data"
                 :columns="columns"
                 :checked-rows.sync="checkedRows"
             
                 checkable>
+            </b-table>
 
-              
+
+            <b-table :data="data">
+
+                <!-- Headers -->
+                <b-table-column field="id" label="ID" width="40" sortable numeric></b-table-column>
+                <b-table-column field="first_name" label="First Name" sortable></b-table-column>
+         
+
+                <!-- Rows -->
+                <template slot="row">
+                    <td> test</td>
+                    <td> test</td>
+
+                </template>
             </b-table>
            
 
@@ -30,10 +44,7 @@ export default {
             return {
                 data : [
                     { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
-                    { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
-                    { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
-                    { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
-                    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
+
                 ],
               
                 columns: [
